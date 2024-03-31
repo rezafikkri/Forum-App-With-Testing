2292e26da8be43f3082375c826d52c76aa36956e
rezafikkri <fikkri.reza@gmail.com>
2024-04-01 00:30:45 +0700

    feat: show data in nav-profile component from authUser state

--- components/nav-profile.jsx
+++ components/nav-profile.jsx
@@ -1,20 +1,27 @@
+'use client';
+
+import { useAppSelector } from "@/hooks/redux-hooks";
+
 export default function NavProfile() {
+  const authUser = useAppSelector((states) => states.authUser);
+
   return (
     <details className="dropdown dropdown-end h-8">
       <summary tabIndex={0} role="button" className="btn btn-sm btn-circle avatar hover:bg-transparent border-0 ring-2 ring-primary-content ring-offset-0">
         <div className="w-7 rounded-full">
-          <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+          {authUser ? (
+            <img alt={authUser.name} src={authUser.avatar} />
+          ) : (
+            <div className="skeleton w-7 h-7 rounded-full"></div>
+          )}
         </div>
       </summary>
       <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-        <li className="menu-title">Title</li>
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
+        <li className="menu-title">
+          {authUser ? `Signed in as ${authUser.email}` : (
+            <div className="skeleton h-4 w-full"></div>
+          )}
         </li>
-        <li><a>Settings</a></li>
         <li><a className="hover:bg-red-50 hover:text-red-800">Logout</a></li>
       </ul>
     </details>
