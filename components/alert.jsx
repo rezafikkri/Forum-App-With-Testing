f2f33451cea158e8775f107d0bef71283bc29f30
rezafikkri <fikkri.reza@gmail.com>
2024-04-02 22:02:54 +0700

    feat: fix alert style

--- components/alert.jsx
+++ components/alert.jsx
@@ -12,15 +12,21 @@ export default function Alert({ message, onClose, type = 'danger' }) {
   const newMessage = firstWordCapitalize + replacedMessage.slice(firstWordIndex);
 
   // generate alert style based on type
-  let alertClasses = 'text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400';
+  let alertClasses = 'text-red-800 bg-red-50 dark:text-red-400';
+  let btnClasses = 'bg-red-50 text-red-500 hover:bg-red-200 dark:text-red-400';
   if (type === 'info') {
-    alertClasses = 'text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400';
+    alertClasses = 'text-blue-800 bg-blue-50 dark:text-blue-400';
+    btnClasses = 'bg-blue-50 text-blue-800 hover:bg-blue-200 dark:text-blue-400';
   }
 
   return (
-    <div className={`flex items-center p-4 mb-2 ${alertClasses}`} role="alert">
-      <div>{newMessage}</div>
-      <button type="button" className="ms-auto -mx-1.5 -my-1.5 bg-red-50 text-red-500 rounded-lg p-1.5 hover:bg-red-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-red-400 dark:hover:bg-gray-700" onClick={onClose}>
+    <div className={`flex items-center p-4 mb-2 rounded-lg dark:bg-gray-800 ${alertClasses}`} role="alert">
+      <div className="me-2">{newMessage}</div>
+      <button
+        type="button"
+        className={`ms-auto -mx-1.5 -my-1.5 rounded-lg p-1.5 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:hover:bg-gray-700 ${btnClasses}`}
+        onClick={onClose}
+      >
         <span className="sr-only">Close</span>
         <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
           <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
