bef47365c98300b57461a38fa31601fb89524693
rezafikkri <fikkri.reza@gmail.com>
2024-04-02 15:56:19 +0700

    feat: filter threads by category

--- app/(main)/page.jsx
+++ app/(main)/page.jsx
@@ -1,10 +1,11 @@
 'use client';
 
 import ThreadsList from '@/components/threads-list';
-import { useAppDispatch, useAppSelector } from '@/hooks/redux-hooks';
-import { asyncPopulateUsersThreadsAndCategories } from '@/lib/shared/action';
 import Link from 'next/link';
 import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '@/hooks/redux-hooks';
+import { asyncPopulateUsersThreadsAndCategories } from '@/lib/shared/action';
+import { setCategoryActionCreator } from '@/lib/categories/action';
 
 export default function Home() {
   const dispatch = useAppDispatch();
@@ -12,6 +13,10 @@ export default function Home() {
   const users = useAppSelector((states) => states.users);
   const threads = useAppSelector((states) => states.threads);
   const categories = useAppSelector((states) => states.categories);
+
+  function handleCategoryChange(e) {
+    dispatch(setCategoryActionCreator(e.target.value));
+  }
 
   useEffect(() => {
     dispatch(asyncPopulateUsersThreadsAndCategories());
@@ -22,6 +27,11 @@ export default function Home() {
     owner: users.find((user) => user.id === thread.ownerId),
   }));
 
+  // if category selected != all
+  if (categories.selected !== 'all') {
+    threadsList = threadsList.filter((thread) => thread.category === categories.selected);
+  }
+
   return (
     <>
       <header className="mt-20 mb-9">
@@ -30,13 +40,23 @@ export default function Home() {
           {authUser && <Link href="/create" className="btn btn-primary btn-sm">Create Thread</Link>}
         </div>
         <div className="flex justify-between items-center">
-          <p>200 threads</p>
+          <p>{threads.length} threads</p>
           <div className="flex items-center">
-            <span className="border-y border-s border-gray-300 bg-gray-50 h-8 flex px-3 items-center text-sm rounded-tl-lg rounded-bl-lg">Category</span>
-            <select className="select select-bordered w-full select-sm rounded-bl-none rounded-tl-none">
-              <option>all</option>
-              <option>General</option>
-              <option>Redux</option>
+            <label
+              className="border-y border-s border-gray-300 bg-gray-50 h-8 flex px-3 items-center text-sm rounded-tl-lg rounded-bl-lg"
+              for="category"
+            >
+              Category
+            </label>
+            <select
+              className="select select-bordered w-full select-sm rounded-bl-none rounded-tl-none"
+              id="category"
+              value={categories.selected}
+              onChange={handleCategoryChange}
+            >
+              {categories.values.map((category) => (
+                <option key={category} value={category}>{category}</option>
+              ))}
             </select>
           </div>
         </div>
