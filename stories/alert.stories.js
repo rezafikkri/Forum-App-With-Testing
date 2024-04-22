a61faae86096c0bcd6d21fc3452a4e764cc3d8b6
rezafikkri <fikkri.reza@gmail.com>
2024-04-22 07:40:04 +0700

    docs: create stories for Alert component

---
+++ stories/alert.stories.js
@@ -0,0 +1,29 @@
+import Alert from '@/components/alert';
+
+export default {
+  title: 'Alert',
+  component: Alert,
+  tags: ['autodocs'],
+  argTypes: {
+    type: {
+      options: ['info', 'danger'],
+      control: { type: 'radio' },
+    },
+  },
+};
+
+export const Info = {
+  args: {
+    message: 'This is info message',
+    onClose: () => {},
+    type: 'info',
+  },
+};
+
+export const Danger = {
+  args: {
+    message: 'This is danger message',
+    onClose: () => {},
+    type: 'danger',
+  },
+}
